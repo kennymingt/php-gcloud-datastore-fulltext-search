5ee08d99a3c275cfd694e0481826b601aaf08f70
Kent Tang Charlo <kennymingt@gmail.com>
2019-04-19 22:05:50 +0800

    Renamed findkeywords to extractKeywords docblock fix bug replacing \r \n Added remove plurals function

--- parseString.php
+++ parseString.php
@@ -1,15 +1,27 @@
 <?php
 namespace keywordEngine;
 
-function findKeywords($string, $omit = null)
+
+/**
+ * Extract keywords from a string
+ * * Puntuation and symbols are removed
+ * * Accented vowels are transformed to regular vowels
+ * * Words shorter than 3 characters are ignored
+ * * Words passed in the omit parameter are ignored
+ *
+ * @param string $string The text string we want to extract keywords from
+ * @param string[] $omit List of words we don't want to convert to keywords
+ * @return string[] List of keyword extracted from the text
+ */
+function extractKeywords($string, $omit = null)
 {
     // to lowercase
     $string = strtolower($string);
 
     // trim, remove returns and symbols
     $string = trim($string);
-    $string = str_replace("\n", "", $string); // Why regex doesn't remove this?
-    $string = str_replace("\r", "", $string);
+    $string = str_replace("\n", " ", $string); // Why regex doesn't remove this?
+    $string = str_replace("\r", " ", $string);
     $string = preg_replace("/[^a-zA-Z0-9ñáéíóúàèìòùäëïöüç\s]/u", " ", $string);
 
 
@@ -48,4 +60,27 @@ function findKeywords($string, $omit = null)
     $keywords = explode(" ", $string);
 
     return $keywords;
+}
+
+/**
+ * Remove the plural from the word
+ *
+ * @param string[] $words List of words to be transformed
+ * @param array[string]string $dict Transformation rules
+ * @return string[] List of transformed words
+ */
+function removePlural($words, $dict){
+    foreach ($dict as $key => $value) {
+
+        foreach ($words as &$word) {
+            // match $key with word ending
+            $wordEnd = \substr($word, -1 * \strlen($key));
+
+            if ($wordEnd == $key) {
+                $word = \substr_replace($word, $value, -1* \strlen($key));
+            }
+        }
+    }
+
+    return $words;
 }
