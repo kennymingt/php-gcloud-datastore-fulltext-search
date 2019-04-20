8d8080a9b58370d49fb21dc9d11db0bd7f7ef526
Kent Tang Charlo <kennymingt@gmail.com>
2019-04-20 20:20:21 +0800

    fixed bug with regex removing short words fixed bug for string where the last word got removed It wasleaving a trailing space, that appears in the array Added load data

--- parseString.php
+++ parseString.php
@@ -3,6 +3,29 @@ namespace keywordEngine;
 
 
 /**
+ * Load data for keyword indexing
+ * TODO: Rethink
+ *
+ * @return void
+ */
+function loadValues()
+{
+
+    // load a list of keywords from a file, It will be used as a list of words to omit from keywords
+    $filename = __DIR__ . "/../data/omit.txt";
+    $omitString = file_get_contents($filename);
+    $omit = \keywordEngine\extractKeywords($omitString);
+
+    $filename = __DIR__ . "/../data/pluralsEs.json";
+    $pluralsContent = file_get_contents($filename);
+
+    $pluralsEs = json_decode($pluralsContent, true);
+
+    return ["omit" => $omit, "plurals" => $pluralsEs];
+}
+
+
+/**
  * Extract keywords from a string
  * * Puntuation and symbols are removed
  * * Accented vowels are transformed to regular vowels
@@ -19,27 +42,28 @@ function extractKeywords($string, $omit = null)
     $string = strtolower($string);
 
     // trim, remove returns and symbols
-    $string = trim($string);
+    // $string = trim($string); // we are trimming at the end 
     $string = str_replace("\n", " ", $string); // Why regex doesn't remove this?
     $string = str_replace("\r", " ", $string);
     $string = preg_replace("/[^a-zA-Z0-9ñáéíóúàèìòùäëïöüç\s]/u", " ", $string);
 
-
+    
     // Replace accent symbols for rgular vowels to index
     $accentLetters = [
         ["á", "é", "í", "ó", "ú"],
         ["à", "è", "ì", "ò", "ù"],
-        ["ä", "ë", "ï", "ö", "ü"],        
+        ["ä", "ë", "ï", "ö", "ü"],
     ];
-    $letters = ["a", "e","i","o","u"];
+    $letters = ["a", "e", "i", "o", "u"];
     foreach ($accentLetters as $arr) {
         $string = str_replace($arr, $letters, $string);
     }
-
-
+    // $string = str_replace("ñ", "n", $string);
+    // Not sure about this one. maybe index it as it is, and change the search string if there are no enough results
+    
     // omit words shorter than 3leters
-    $string = preg_replace('/(\b[^a-zA-Z0-9ñáéíóúàèìòùäëïöüç].{1,2}\b)/u',' ',$string);
-
+    $string = preg_replace('/\b[a-z0-9ñáéíóúàèìòùäëïöüç]{1,2}\b/ui', ' ', $string);
+    // $string = preg_replace('/\b\w{1,2}\b/i',' ',$string);
 
     // omit undesired words 
     // foreach ($omit as &$word) {
@@ -48,14 +72,17 @@ function extractKeywords($string, $omit = null)
 
     // $string = preg_replace($omit, " ", $string);
 
-    if($omit != null) {
+    if ($omit != null) {
         $string = preg_replace('/\b(' . implode('|', $omit) . ')\b/u', '', $string);
     }
 
-
-
     // remove double spaces
     $string = preg_replace("/\s+/", " ", $string);
+
+
+    // Sometimes, after removing words, there is a white space at the end, explode will create an entry for it
+    $string = \trim($string);
+
 
     $keywords = explode(" ", $string);
 
@@ -69,7 +96,8 @@ function extractKeywords($string, $omit = null)
  * @param array[string]string $dict Transformation rules
  * @return string[] List of transformed words
  */
-function removePlural($words, $dict){
+function removePlural($words, $dict)
+{
     foreach ($dict as $key => $value) {
 
         foreach ($words as &$word) {
@@ -77,7 +105,7 @@ function removePlural($words, $dict){
             $wordEnd = \substr($word, -1 * \strlen($key));
 
             if ($wordEnd == $key) {
-                $word = \substr_replace($word, $value, -1* \strlen($key));
+                $word = \substr_replace($word, $value, -1 * \strlen($key));
             }
         }
     }
