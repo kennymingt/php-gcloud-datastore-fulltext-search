815eafe9b7a48efe934ba3e3824896d29ab7b8df
Kent Tang Charlo <kennymingt@gmail.com>
2019-04-20 20:21:19 +0800

    added indexEntity, searchIndex, deleteIndex

--- keywordEngine.php
+++ keywordEngine.php
@@ -2,12 +2,40 @@
 namespace keywordEngine;
 
 use Google\Cloud\Datastore\DatastoreClient;
+use Google\Cloud\Datastore\Transaction;
 use Google\cloud\Datastore\Key;
+
+require __DIR__ . "/parseString.php";
+
+/**
+ * Index an entity for search by keyword
+ *
+ * @param Transaction $tr Datastore client
+ * @param Key $key Entity key
+ * @param string $string string to index
+ * @return void
+ */
+function indexEntity($tr, $key, $string)
+{
+
+    // Load list of words to omit whil extracting keywords
+    // Also load the list of plurals rules
+    // TODO: Should I decouple it? pass it as param?
+    $values = loadValues();
+    $omit = $values["omit"];
+    $pluralsEs = $values["plurals"];
+
+    $keywords = \keywordEngine\extractKeywords($string, $omit);
+    $keywords = \keywordEngine\removePlural($keywords, $pluralsEs);
+
+    \keywordEngine\index($tr, $key, $keywords);
+}
 
 
 /**
  * Indexes a datastore key, and their related list of keywords
  *
+ * @access private
  * @param DatastoreClient $ds Datastore Client
  * @param Key $key Key of the enity we want to index
  * @param string[] $keywords List of keywords
@@ -32,9 +60,36 @@ function index($ds, $key, $keywords)
 }
 
 
+
+
+/**
+ * Search the index by a string
+ *
+ * @param string $search
+ * @return Key[] returns the Id of the matching entities
+ */
+function searchIndex($search)
+{
+    // Load list of words to omit whil extracting keywords
+    // Also load the list of plurals rules
+    // Should I decouple it? pass it as param?
+    $values = loadValues();
+    $omit = $values["omit"];
+    $pluralsEs = $values["plurals"];
+
+    $searchKeywords = \keywordEngine\extractKeywords($search, $omit);
+    $searchKeywords = \keywordEngine\removePlural($searchKeywords, $pluralsEs);
+
+
+    $keys = \keywordEngine\searchByKeywords($searchKeywords);
+
+    return $keys;
+}
+
 /**
  * Search the datastore looking for keywords
  *
+ * @access private 
  * @param string[] $keywords List of keywords to search for
  * @return string[] List of entityIds of matching records
  */
@@ -59,4 +114,22 @@ function searchByKeywords($keywords)
     };
 
     return $keys;
+}
+
+
+
+/**
+ * Delete an indexed entity
+ *
+ * @param Transation $tr
+ * @param Key $key
+ * @return void
+ */
+function deleteIndex($tr, $key)
+{
+    $ds = new DatastoreClient();
+    $id = $key->pathEndIdentifier();
+    $indexKey = $ds->key("keywordIndex", $id)->ancestorKey($key);
+
+    $tr->delete($indexKey);
 }
